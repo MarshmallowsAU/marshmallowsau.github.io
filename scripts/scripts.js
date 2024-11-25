b633b312aa6935af42ddf3fa12ed58621fb5f2bb
Jerry M Yang <jmax.yang@gmail.com>
2024-11-25 21:13:09 +1100

    Refactored Projects Page | Fixed Issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ function scrollInto(element) {
 
 function showDropDown() {
     if (document.getElementById("drop1").style.display == "block") {
-        document.getElementById("drop-parent").innerHTML = '<a href="#nothing" style="color: inherit;"><i class="bi bi-list"></i></a>';
+        document.getElementById("drop-parent").innerHTML = '<a style="color: inherit;"><i class="bi bi-list"></i></a>';
         closeDropdown();
         for (const id of ["drop1", "drop2", "drop3", "drop4", "drop5", "drop6"]) {
             document.getElementById(id).style.display = "none";
@@ -22,11 +22,10 @@ function showDropDown() {
         for (const id of ["drop1", "drop2", "drop3", "drop4", "drop5", "drop6"]) {
             document.getElementById(id).style.display = "block";
         }
-        document.getElementById("drop-parent").innerHTML = '<a href="#nothing" style="color: inherit;"><i class="bi bi-x"></i></a>';
+        document.getElementById("drop-parent").innerHTML = '<a style="color: inherit;"><i class="bi bi-x"></i></a>';
         openDropdown();
     }
 }
-
 
 function closeDropdown() {
     const dropdowns = document.querySelectorAll('.float-dropdown-open');
@@ -48,19 +47,50 @@ function openDropdown() {
 
 function darkModeToggle() {
     const body = document.getElementById("main-body");
+    const footer = document.getElementById("#bottom")
     if (body.className == "body-light") {
         body.classList.remove('body-light');
         body.className = 'body';
-        document.getElementById("dark-mode-button").innerHTML = '<a href="#nothing" style="color: inherit;"><i class="bi bi-brightness-high-fill"></i></a>';
-        for (const id of ["float", "float-dropdown-parent", "float-dropdown", "float-dropdown-open", "container", "resume"]) {
-            removeInvertByClass(id);
+        footer.classList.remove('footer-light');
+        footer.className = 'footer-dark';
+        document.getElementById("avatar").innerHTML = '<img src="assets/Profile.jpg" alt="Avatar" class="avatar"></img>'
+        document.getElementById("dark-mode-button").innerHTML = '<a style="color: inherit;"><i class="bi bi-brightness-high-fill"></i></a>';
+        for (const xclass of ["float", "float-dropdown-parent", "float-dropdown", "float-dropdown-open", "container", "resume", "avatar", "footer-container"]) {
+            removeInvertByClass(xclass);
         }
     } else {
         body.classList.remove('body');
         body.className = 'body-light';
-        document.getElementById("dark-mode-button").innerHTML = '<a href="#nothing" style="color: inherit;"><i class="bi bi-moon-fill"></i></a>';
-        for (const id of ["float", "float-dropdown-parent", "float-dropdown", "float-dropdown-open", "container", "resume"]) {
-            invertElementsByClass(id);
+        footer.classList.remove('footer-dark');
+        footer.className = 'footer-light';
+        document.getElementById("avatar").innerHTML = '<img src="assets/Profile.jpg" alt="Avatar" class="avatar-light"></img>'
+        document.getElementById("dark-mode-button").innerHTML = '<a style="color: inherit;"><i class="bi bi-moon-fill"></i></a>';
+        for (const xclass of ["float", "float-dropdown-parent", "float-dropdown", "float-dropdown-open", "container", "resume", "avatar", "footer-container"]) {
+            invertElementsByClass(xclass);
+        }
+    }
+}
+
+function darkModeToggleProject() {
+    const body = document.getElementById("main-body");
+    const footer = document.getElementById("#bottom")
+    if (body.className == "body-light") {
+        body.classList.remove('body-light');
+        body.className = 'body';
+        footer.classList.remove('footer-light');
+        footer.className = 'footer-dark';
+        document.getElementById("dark-mode-button").innerHTML = '<a style="color: inherit;"><i class="bi bi-brightness-high-fill"></i></a>';
+        for (const xclass of ["float", "float-dropdown-parent", "float-dropdown", "float-dropdown-open", "container", "resume", "avatar", "footer-container"]) {
+            removeInvertByClass(xclass);
+        }
+    } else {
+        body.classList.remove('body');
+        body.className = 'body-light';
+        footer.classList.remove('footer-dark');
+        footer.className = 'footer-light';
+        document.getElementById("dark-mode-button").innerHTML = '<a style="color: inherit;"><i class="bi bi-moon-fill"></i></a>';
+        for (const xclass of ["float", "float-dropdown-parent", "float-dropdown", "float-dropdown-open", "container", "resume", "avatar", "footer-container"]) {
+            invertElementsByClass(xclass);
         }
     }
 }
@@ -91,12 +121,26 @@ function toggleCourses() {
         courses.className = 'study-collapsible';
         courses.style.display = "none";
         document.getElementById("show-hide-button").innerHTML = 'Show Courses';
-        document.body.style.height = '4560px';
+        document.body.style.height = '4975px';
     } else {
         courses.style.display = "block";
         courses.classList.remove('study-collapsible');
         courses.className = 'study-collapsible-open';
         document.getElementById("show-hide-button").innerHTML = 'Hide Courses';
-        document.body.style.height = '5030px';
+        document.body.style.height = '5445px';
     }
 }
+
+/** Image Gallery **/
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
