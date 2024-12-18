6264f4707fc3f1e70a0efe2fe631d07ba8681d2a
Jerry M Yang <jmax.yang@gmail.com>
2024-12-18 17:04:16 +1100

    Modified Body Height to Auto, Added Projects JSON, Removed P5

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -121,13 +121,11 @@ function toggleCourses() {
         courses.className = 'study-collapsible';
         courses.style.display = "none";
         document.getElementById("show-hide-button").innerHTML = 'Show Courses';
-        document.body.style.height = '4975px';
     } else {
         courses.style.display = "block";
         courses.classList.remove('study-collapsible');
         courses.className = 'study-collapsible-open';
         document.getElementById("show-hide-button").innerHTML = 'Hide Courses';
-        document.body.style.height = '5445px';
     }
 }
 
