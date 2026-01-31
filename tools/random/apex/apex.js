cd4065690738a58a27548f1a8187bb9ea6447f94
Jerry M Yang <jmax.yang@gmail.com>
2026-01-31 22:16:34 +1100

    Fixed images not loading...

--- tools/random/apex/apex.js
+++ tools/random/apex/apex.js
@@ -22,30 +22,30 @@ function roll() {
     if (characterChecked & loadoutChecked) {
         resultDiv.innerHTML = `<div class="row">
           <div class="column-chr">
-            <div id="char_results"><img src="apex/assets/characters/char${char}.png"</img></div>
+            <div id="char_results"><img src="/tools/random/apex/assets/characters/char${char}.png"</img></div>
           </div>
           <div class="column-wep">
             <div class="column-wep-row">
-                <div id="wep1_results"><img src="apex/assets/weapons/wep${first}.png"</img></div>
+                <div id="wep1_results"><img src="/tools/random/apex/assets/weapons/wep${first}.png"</img></div>
             </div>
             <div class="column-wep-row">
-                <div id="wep2_results"><img src="apex/assets/weapons/wep${second}.png"</img></div>
+                <div id="wep2_results"><img src="/tools/random/apex/assets/weapons/wep${second}.png"</img></div>
             </div>
           </div>
         </div`
     } else if (characterChecked) {
         resultDiv.innerHTML = `<div class="row">
             <div class="column-full-chr">
-                <div id="char_results"><img src="apex/assets/characters/char${char}.png"</img></div>
+                <div id="char_results"><img src="/tools/random/apex/assets/characters/char${char}.png"</img></div>
             </div>
           </div>`
     } else if (loadoutChecked) {
         resultDiv.innerHTML = `<div class="row-full-wep">
             <div class="column-full-wep">
-                <div id="wep1_results"><img src="apex/assets/weapons/wep${first}.png"</img></div>
+                <div id="wep1_results"><img src="/tools/random/apex/assets/weapons/wep${first}.png"</img></div>
             </div>
             <div class="column-full-wep">
-                <div id="wep2_results"><img src="apex/assets/weapons/wep${second}.png"</img></div>
+                <div id="wep2_results"><img src="/tools/random/apex/assets/weapons/wep${second}.png"</img></div>
             </div>
         </div>`
     }
